49312c52f291c5e5942b3e8516f468e6f2a9a7e9
Cade Michael <cade.lueker@gmail.com>
2022-04-12 23:23:29 -0400

    cleanup frontend

--- flaskVersion/main.py
+++ flaskVersion/main.py
@@ -1,5 +1,5 @@
 # module imports 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 import os 
 # local imports 
 from formPref import PreferenceForm
@@ -9,10 +9,12 @@ app.config['SECRET_KEY'] = os.urandom(16)
 
 @app.route('/')
 def home():
+    """ Render information on the project """
     return render_template('home.html')
 
 @app.route('/preferences', methods=["GET","POST"])
 def preferences():
+    """ Take in user preferences and return matches """
     pForm = PreferenceForm()
     if request.method == "POST":
         # populate from pForm
@@ -34,16 +36,15 @@ def preferences():
                 pField = pField,
                 eLevel = eLevel,
                 eMajor = eMajor,
-                prefs=True
                 )
     return render_template(
             'preferences.html',
             form=pForm,
             )
 
-@app.route('/matches')
-def matches():
-    return render_template('matches.html', prefs=False)
+# @app.route('/matches')
+# def matches():
+#     return render_template('matches.html', prefs=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
