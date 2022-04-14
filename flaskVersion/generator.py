2d77a660bd2abe2bb6b2274f992db6e2960a891c
Cade Michael <cade.lueker@gmail.com>
2022-04-14 15:53:19 -0400

    hobby table and querying

--- flaskVersion/generator.py
+++ flaskVersion/generator.py
@@ -1,37 +1,22 @@
 import random
 
-def randNames(f_names, l_names): 
-    f_names = f_names.copy()
-    l_names = l_names.copy()
-    names = []
-    id = 0
-    while(len(f_names) > 0):
-        f_index = random.randint(0, len(f_names) - 1)
-        l_index = random.randint(0, len(l_names) - 1)
-        f = f_names.pop(f_index)
-        l = l_names.pop(l_index)
-        age = random.randint(18,50)
-        names.append((id,f,l,age))
-        id = id + 1
-    return names
-
 f_names = [
-        'cade',
-        'john',
-        'mary',
-        'patricia',
-        'lisa',
-        'charles',
-        'jennifer',
-        'thomas',
-        'stephanie',
-        'will',
-        'joe',
-        'betty',
-        'grace',
-        'matthew',
-        'mark',
-        'quin']
+            'cade',
+            'john',
+            'mary',
+            'patricia',
+            'lisa',
+            'charles',
+            'jennifer',
+            'thomas',
+            'stephanie',
+            'will',
+            'joe',
+            'betty',
+            'grace',
+            'matthew',
+            'mark',
+            'quin']
 l_names = [
         'smith',
         'williams',
@@ -48,9 +33,24 @@ l_names = [
         'edwards',
         'doe',
         'mitchel',
-        'lafrance',
-        ]
+        'lafrance']
+
+def randNames(f_names, l_names): 
+    f_names = f_names.copy()
+    l_names = l_names.copy()
+    names = []
+    id = 0
+    while(len(f_names) > 0):
+        f_index = random.randint(0, len(f_names) - 1)
+        l_index = random.randint(0, len(l_names) - 1)
+        f = f_names.pop(f_index)
+        l = l_names.pop(l_index)
+        age = random.randint(18,50)
+        names.append((id,f,l,age))
+        id = id + 1
+    return names
+
 
 people = randNames(f_names, l_names)
 
-print(people)
+# print(people)
