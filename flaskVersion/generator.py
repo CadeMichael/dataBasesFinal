884173e0b8f0c519755573ad2c24efbd3314e112
Cade Michael <cade.lueker@gmail.com>
2022-04-14 15:06:04 -0400

    multiple insertions and generate with id

--- flaskVersion/generator.py
+++ flaskVersion/generator.py
@@ -4,13 +4,15 @@ def randNames(f_names, l_names):
     f_names = f_names.copy()
     l_names = l_names.copy()
     names = []
+    id = 0
     while(len(f_names) > 0):
         f_index = random.randint(0, len(f_names) - 1)
         l_index = random.randint(0, len(l_names) - 1)
         f = f_names.pop(f_index)
         l = l_names.pop(l_index)
         age = random.randint(18,50)
-        names.append((f,l,age))
+        names.append((id,f,l,age))
+        id = id + 1
     return names
 
 f_names = [
